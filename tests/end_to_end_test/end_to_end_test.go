3f9fa65d5bf4c523ca3e352d8586644f056cbb2e
Julio Lopez <julio+gh@kasten.io>
2020-01-05 21:28:56 -0800

    Fix assumptions in E2E GC test

--- tests/end_to_end_test/end_to_end_test.go
+++ tests/end_to_end_test/end_to_end_test.go
@@ -294,7 +294,9 @@ how are you
 	e.runAndExpectSuccess(t, "snapshot", "gc", "--delete", "--min-age", "0s")
 
 	// two contents are deleted
-	expectedContentCount -= 2
+	// the gc-mark phase should produce 2 new contents: one for the gc-mark
+	// details and one for the gc manifest, so the net number of expected
+	// contents should stay the same.
 	e.runAndVerifyOutputLineCount(t, expectedContentCount, "content", "list")
 }
 
