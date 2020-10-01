8f4f59204f26b7d12f6b63ddbfa49de9799c6d3d
Nick <nwright970@gmail.com>
2020-10-01 00:18:06 +0000

    Remove unused test

--- tests/robustness/robustness_test/robustness_test.go
+++ tests/robustness/robustness_test/robustness_test.go
@@ -82,30 +82,6 @@ func TestManySmallFilesAcrossDirecoryTree(t *testing.T) {
 	testenv.AssertNoError(t, err)
 }
 
-// func TestRandomized(t *testing.T) {
-// 	st := time.Now()
-
-// 	opts := engine.ActionOpts{
-// 		engine.ActionControlActionKey: map[string]string{
-// 			string(engine.SnapshotRootDirActionKey):          strconv.Itoa(2),
-// 			string(engine.RestoreRandomSnapshotActionKey):    strconv.Itoa(2),
-// 			string(engine.DeleteRandomSnapshotActionKey):     strconv.Itoa(1),
-// 			string(engine.WriteRandomFilesActionKey):         strconv.Itoa(8),
-// 			string(engine.DeleteRandomSubdirectoryActionKey): strconv.Itoa(1),
-// 		},
-// 		engine.WriteRandomFilesActionKey: map[string]string{
-// 			engine.IOLimitPerWriteAction: fmt.Sprintf("%d", 1*1024*1024*1024),
-// 		},
-// 	}
-
-// 	// Perform actions until the timer expires, at least until one action
-// 	// has been performed
-// 	for time.Since(st) <= *randomizedTestDur || eng.RunStats.ActionCounter == 0 {
-// 		err := eng.RandomAction(opts)
-// 		testenv.AssertNoError(t, err)
-// 	}
-// }
-
 func TestRandomizedSmall(t *testing.T) {
 	st := time.Now()
 
