b67ea18fbf1572212a0340fe68571d4a4f774f93
Julio Lopez <julio+gh@kasten.io>
2021-04-07 21:16:32 -0700

    cli: add pit flag support

---
+++ cli/pit.go
@@ -0,0 +1,34 @@
+package cli
+
+import (
+	"time"
+
+	"github.com/alecthomas/kingpin"
+	"github.com/araddon/dateparse"
+	"github.com/pkg/errors"
+)
+
+var pointInTime time.Time
+
+func registerPointInTimeFlag(cmd *kingpin.CmdClause) {
+	addPointInTimeFlag(cmd, &pointInTime)
+}
+
+func addPointInTimeFlag(cmd *kingpin.CmdClause, pit *time.Time) {
+	var pointInTimeStr string
+
+	pitPreAction := func(pc *kingpin.ParseContext) error {
+		if pointInTimeStr != "" {
+			t, err := dateparse.ParseStrict(pointInTimeStr)
+			if err != nil {
+				return errors.Wrap(err, "invalid point-in-time argument")
+			}
+
+			*pit = t
+		}
+
+		return nil
+	}
+
+	cmd.Flag("point-in-time", "Use a point-in-time view of the storage repository when supported").PlaceHolder(time.RFC3339).PreAction(pitPreAction).StringVar(&pointInTimeStr)
+}
