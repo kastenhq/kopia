2938f776a5b9e84c40dd1cedf9b0e94694ff2d3e
Julio Lopez <julio+gh@kasten.io>
2021-04-12 16:23:09 -0700

    Validate whether bucket supports versioning when specifying PIT

--- repo/blob/s3/s3_storage.go
+++ repo/blob/s3/s3_storage.go
@@ -19,6 +19,7 @@ import (
 	"github.com/pkg/errors"
 
 	"github.com/kopia/kopia/repo/blob"
+	"github.com/kopia/kopia/repo/blob/pit"
 	"github.com/kopia/kopia/repo/blob/retrying"
 )
 
@@ -252,13 +253,18 @@ func New(ctx context.Context, opt *Options) (blob.Storage, error) {
 		return nil, errors.Errorf("bucket %q does not exist", opt.BucketName)
 	}
 
-	return retrying.NewWrapper(&s3Storage{
+	s, err := pit.NewWrapper(ctx, &s3Storage{
 		Options:           *opt,
 		cli:               cli,
 		sendMD5:           0,
 		downloadThrottler: downloadThrottler,
 		uploadThrottler:   uploadThrottler,
-	}), nil
+	}, opt.StoreOptions.PointInTimeView)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not create PIT storage")
+	}
+
+	return retrying.NewWrapper(s), nil
 }
 
 func init() {
