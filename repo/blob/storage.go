71f0d220fb55efd5d8e021788a26a75475c3afc3
Onkar Bhat <onkar@kasten.io>
2021-10-29 18:27:17 -0700

    Add comment for the new error.

--- repo/blob/storage.go
+++ repo/blob/storage.go
@@ -19,6 +19,8 @@ var ErrSetTimeUnsupported = errors.Errorf("SetTime is not supported")
 // ErrInvalidRange is returned when the requested blob offset or length is invalid.
 var ErrInvalidRange = errors.Errorf("invalid blob offset or length")
 
+// ErrTokenExpired is returned when the token used for
+// authenticating with storage provider has expired.
 var ErrTokenExpired = errors.Errorf("The provided token has expired")
 
 // Bytes encapsulates a sequence of bytes, possibly stored in a non-contiguous buffers,
