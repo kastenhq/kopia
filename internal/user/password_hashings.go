5e19f56c31c22564c87f78351e497268fdcbbf7e
Julio <1953782+julio-lopez@users.noreply.github.com>
2024-05-24 18:20:11 -0700

    remove unused PasswordHashingAlgorithms

--- internal/user/password_hashings.go
+++
@@ -1,6 +0,0 @@
-package user
-
-// PasswordHashingAlgorithms returns the supported algorithms for user password hashing.
-func PasswordHashingAlgorithms() []string {
-	return []string{scryptHashAlgorithm, pbkdf2HashAlgorithm}
-}
