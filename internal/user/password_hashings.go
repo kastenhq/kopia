9cd8c6767bd31db7b1ee4fa04dbb18d4bd79c865
Julio <1953782+julio-lopez@users.noreply.github.com>
2024-05-24 18:18:06 -0700

    remove unused DefaultPasswordHashingAlgorithm const

--- internal/user/password_hashings.go
+++ internal/user/password_hashings.go
@@ -1,8 +1,5 @@
 package user
 
-// DefaultPasswordHashingAlgorithm is the default password hashing scheme for user profiles.
-const DefaultPasswordHashingAlgorithm = scryptHashAlgorithm
-
 // PasswordHashingAlgorithms returns the supported algorithms for user password hashing.
 func PasswordHashingAlgorithms() []string {
 	return []string{scryptHashAlgorithm, pbkdf2HashAlgorithm}
