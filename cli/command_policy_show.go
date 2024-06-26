f40c9991f2473cd1555d257aab49dcad7664d3b7
Prasad Ghangal <prasad.ganghal@veeam.com>
2024-06-26 15:46:57 +0530

    Add metadata compression to set and show policy
    Signed-off-by: Prasad Ghangal <prasad.ganghal@veeam.com>

--- cli/command_policy_show.go
+++ cli/command_policy_show.go
@@ -126,6 +126,8 @@ func printPolicy(out *textOutput, p *policy.Policy, def *policy.Definition) {
 	rows = append(rows, policyTableRow{})
 	rows = appendCompressionPolicyRows(rows, p, def)
 	rows = append(rows, policyTableRow{})
+	rows = appendMetadataCompressionPolicyRows(rows, p, def)
+	rows = append(rows, policyTableRow{})
 	rows = appendSplitterPolicyRows(rows, p, def)
 	rows = append(rows, policyTableRow{})
 	rows = appendActionsPolicyRows(rows, p, def)
@@ -385,6 +387,18 @@ func appendCompressionPolicyRows(rows []policyTableRow, p *policy.Policy, def *p
 		rows = append(rows, policyTableRow{"  Compress files of all sizes.", "", ""})
 	}
 
+	return rows
+}
+
+func appendMetadataCompressionPolicyRows(rows []policyTableRow, p *policy.Policy, def *policy.Definition) []policyTableRow {
+	if p.MetadataCompressionPolicy.CompressorName == "" || p.MetadataCompressionPolicy.CompressorName == "none" {
+		rows = append(rows, policyTableRow{"Compression disabled.", "", ""})
+		return rows
+	}
+
+	rows = append(rows,
+		policyTableRow{"Compression:", "", ""},
+		policyTableRow{"  Compressor:", string(p.MetadataCompressionPolicy.CompressorName), definitionPointToString(p.Target(), def.MetadataCompressionPolicy.CompressorName)})
 	return rows
 }
 
